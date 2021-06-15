43f17c8fe3fd9cfedbb34a136cf9ccc5f6986616
Yi-Chang Chen <yichang.thomas@gmail.com>
2021-06-15 16:46:45 +0100

    add filter.

--- read.py
+++ read.py
@@ -10,7 +10,16 @@ with open('reviews.txt', 'r') as f:
 print('檔案讀取完了，總共有', len(data), '筆資料')
 
 sum_len = 0
-for d in data:						#讀取清單（data）存到 d 裡。
+for d in data:						#for loop 的意思就是把清單（data）的東西一個個拿出來，存到 d 裡。
 	sum_len = sum_len + len(d)		#運用len 來計算 d 的長度然而把它加到sum_len 裡。
 
 print('留言的平均長度為', sum_len / len(data))		#sum_len 己經計算出總文字的數目在除以 len(data)(留言的筆目，共100萬筆資料)。
+
+
+new =[]
+for d in data:						#for loop 的意思就是把清單（data）的東西一個個拿出來，存到 d 裡。
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留這長度小於100')
+print(new[0])
+print(new[2])
