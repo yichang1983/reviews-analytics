fb72b4020dc0e5f5d593c299cbc728dbb04fea9e
Yi-Chang Chen <yichang.thomas@gmail.com>
2021-06-15 16:01:36 +0100

    amend some words

--- read.py
+++ read.py
@@ -6,8 +6,5 @@ with open('reviews.txt', 'r') as f:
 		count += 1					#count = count +1
 		if count % 10000 == 0:		# %是取餘數，意思就是如果count /1000 的餘數等於零，則印出來。
 			print(len(data))
-print(len(data))
 
-print(data[0])
-print('----------------')
-print(data[1])
+print('檔案讀取完了，總共有', len(data), '筆資料')
