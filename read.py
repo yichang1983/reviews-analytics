433c30dc32eb743e89c2741294d0e237b400b182
Yi-Chang Chen <yichang.thomas@gmail.com>
2021-06-24 16:27:45 +0100

    add word count funtion

--- read.py
+++ read.py
@@ -32,3 +32,43 @@ for d in data:
 print('一共有', len(good), '筆留這長度提到good')
 print(good[0])
 print(good[100])
+
+
+#進階寫法(清單快寫法：影片 057)
+good = [d for d in data if 'good' in d]			#等於29-31行的合併，而剛開始的d 就是good.append(d).
+print('一共有', len(good), '筆留這長度提到good')
+print(good[0])
+print(good[100])
+
+
+#文字計數
+
+word_count = {}				#建立一個字典
+
+for d in data:				#從1百萬筆清單(data)中 讀取，然而每一筆存成 d
+	words = d.split(' ')	# d.split(' ') , 的意思是指在這一筆的字串（整篇文章它算是一筆字串）中，如果有空格就切一刀，讓每個單字變成一個字串。
+	for word in words:		# words 是（第上面一行（18行））清單，裡面裝著很多字串，然而從這個清單（words）中讀取，然而每一個字存成word.
+		if word in word_count:		#如果這個字(word)有出現在字典（word_count）裡。
+			word_count[word] += 1	#把目前的次數加1
+		else:						#如果沒有出現過
+			word_count[word] = 1	#把它新增進字典（word_count），然而它的次數是1。
+
+for word in word_count:				#從字典（word_count）裡讀取，每一個key,然而存成word.
+	if word_count[word] > 1000000:
+		print(word, word_count[word])	#印出key(word), 也印出查找出來的次數也印出來(world_count[word])
+
+
+print(len(word_count))
+print(word_count['Thomas'])
+
+while True:
+	word = input('請問你想查什麼字: ')
+	if word == 'q':
+		break
+	if word in word_count:
+		print(word,'出現過的次數為：', word_count[word])
+	else:
+		print('這個字沒有出現過喔！')
+
+print('感謝使用本查詢功能')
+
